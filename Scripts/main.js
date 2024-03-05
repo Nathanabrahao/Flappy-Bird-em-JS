053a1638625bfefb9b3fb11c9b9af7a268c9de7a
Nathanabrahao <nathanntc6@gmail.com>
2024-03-04 23:00:08 -0300

    first commit

---
+++ Scripts/main.js
@@ -0,0 +1,16 @@
+const valorCano = document.querySelectorAll(".cano")
+const passarinho = document.querySelectorAll(".passaro")
+const iniciaGame = document.querySelector(".iniciar")
+
+
+
+console.log(valorCano)
+
+
+iniciaGame.addEventListener('click', () => {
+    valorCano[0].style.left = '0'
+}
+)
+
+
+
