ee15a728228588a6b6167f74efb3c9c76b7a05d8
Nathan_Abrahão <54583223+Nathanabrahao@users.noreply.github.com>
2024-03-05 15:02:14 -0300

    Script main.js

--- Scripts/main.js
+++ Scripts/main.js
@@ -1,32 +1,30 @@
-const valorCano = document.querySelectorAll(".cano")
-const passarinho = document.querySelectorAll(".passaro")
-const iniciaGame = document.querySelector(".iniciar")
-const reinicia = document.querySelector(".reset")
+const valorCano = document.querySelectorAll(".cano");
+const passarinho = document.querySelectorAll(".passaro");
+const iniciaGame = document.querySelector(".iniciar");
+const reinicia = document.querySelector(".reset");
+const somaCanos = document.querySelector(".canos")
+
+
+iniciaGame.addEventListener("click", () => {
+  valorCano[0].style.left = "0";
+  passarinho[0].style.top = "100%";
+  iniciaGame.style.display = "none";
+  start();
+  addCano(canos);
+});
 
 
 
-iniciaGame.addEventListener('click', () => {
-    valorCano[0].style.left = '0';
-    passarinho[0].style.top = '100%';
-    iniciaGame.style.display = "none";
-    start();
+reinicia.addEventListener("click", () => {
+  reset();
+});
+
+
+
+function addCano(iniciaGame) {
+  if (iniciaGame.style.display === "none") {
+    document.canos.innerHTML = `<img src="./img/super-mario-brothers-147465_960_720.jpg" alt="cano2" class="cano2">`;
+  }
 }
-)
-
-
-reinicia.addEventListener('click', () =>{
-    reset()
-})
-
-function addCano(iniciaGame){
-    if(iniciaGame.style.display === "none"){
-        
-    }
-}
-
-
-
-
-
 
 
